4553921672b8b565f82bf2508a7da58ba2911fe2
Pedro <pmfrp@hotmail.com>
2025-01-29 08:23:31 -0300

    Adiciona arquivos de tema WordPress: header.php, footer.php, contato.php, sobre.php, e index.php

--- meu-tema/header.php
+++ meu-tema/header.php
@@ -1,30 +1,30 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title><?php bloginfo('name'); ?></title>
 
-<head>
-    <meta charset="utf-8">
-    <title><?php bloginfo('name');?></title>
+		<link href='https://fonts.googleapis.com/css?family=Alegreya+SC' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
 
-    <link href='https://fonts.googleapis.com/css?family=Alegreya+SC' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
-    <!-- Header Wordpress -->
-    <?php wp_head(); ?>
-    <!-- Header Wordpress -->
-</head>
+		<!-- Header Wordpress -->
+		<?php wp_head(); ?>
+		<!-- Fecha Header Wordpress -->
+	</head>
 
-<body>
+	<body>
+		
+		<header>
+			<nav>
+				<ul>
+					<li class="current_page_item"><a href="index.html">Menu</a></li>
+					<li><a href="sobre.html">Sobre</a></li>
+					<li><a href="contato.html">Contato</a></li>
+				</ul>
+			</nav>
 
-    <header>
-        <nav>
-            <ul>
-                <li class="current_page_item"><a href="index.php">Menu</a></li>
-                <li><a href="sobre.php">Sobre</a></li>
-                <li><a href="contato.php">Contato</a></li>
-            </ul>
-        </nav>
+			<h1><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/rest.png" alt="Rest"></h1>
 
-        <h1><img src="<?php echo get_stylesheet_directory_uri() ?>/img/rest.png" alt="Rest"></h1>
-    
-        <p>Rua Marechal 29 – Copacabana – Rj</p>
-        <p class="telefone">2422-9201</p>
-    </header>
+			<p>Rua Marechal 29 – Copacabana – Rj</p>
+			<p class="telefone">2422-9201</p>
+		</header>
