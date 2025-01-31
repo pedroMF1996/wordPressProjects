439c633e337e1fa6043b2fee9c90edc2a391b9c1
Pedro <pmfrp@hotmail.com>
2025-01-31 19:15:16 -0300

    Update theme files and remove contato.html

--- meu-tema/index.php
+++ meu-tema/index.php
@@ -1,3 +1,15 @@
 <?php get_header(); ?>
-<p>usanto template index</p>
+<section class="container sobre">
+    <?php 
+        if(have_posts()) : while (have_posts()) : the_post();
+    ?>
+        <h2 class="subtitulo"><?php the_title(); ?></h2>
+        <div class="grid-8">
+            <?php the_content(); ?>
+        </div>
+    <?php endwhile; else : ?>
+        <p>Nenhum post encontrado</p>
+    <?php endif; ?>
+</section>
+
 <?php get_footer(); ?>
