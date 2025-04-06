33a04b84e2656f43ec84ac109f6ae73ee4551cc5
Pedro <pmfrp@hotmail.com>
2025-04-06 18:33:07 -0300

    Atualização do tema Bikcraft: configuração do WordPress e estilos

--- meu-tema/page-contato.php
+++ meu-tema/page-contato.php
@@ -2,20 +2,18 @@
 <html lang="pt-br">
 	<head>
 		<meta charset="utf-8">
-		<title>Bikcraft - Bicicletas Personalizadas</title>
+		<title>Bikcraft - Contato - 21 9999-9999</title>
 		<meta name="description" content="Compre a sua bicicleta personalizada na Bikcraft. Possuímos modelos Passeio, Retrô e Esporte.">
 
 		<meta property="og:type" content="website"/>
-		<meta property="og:title" content="Bikcraft - Bicicletas Personalizadas"/>
+		<meta property="og:title" content="Bikcraft - Contato - 21 9999-9999"/>
 		<meta property="og:description" content="Compre a sua bicicleta personalizada na Bikcraft. Possuímos modelos Passeio, Retrô e Esporte."/>
-		<meta property="og:url" content="http://bikcraft.com"/>
+		<meta property="og:url" content="http://bikcraft.com/contato.html"/>
 		<meta property="og:image" content="http://bikcraft.com/img/og-image.png"/>
 
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<link rel="shortcut icon" href="favicon.ico">
-
-		<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/style.css">
+		<link rel="stylesheet" href="css/style.css">
 		<script src="js/libs/modernizr.custom.45655.js"></script>
 	</head>
 	<body>
@@ -30,97 +28,56 @@
 						<li><a href="sobre.html">Sobre</a></li>
 						<li><a href="produtos.html">Produtos</a></li>
 						<li><a href="portfolio.html">Portfólio</a></li>
-						<li><a href="contato.html">Contato</a></li>
+						<li><a href="contato.html" class="menu_ativo">Contato</a></li>
 					</ul>
 				</nav>
 			</div>
 		</header>
 
-		<section class="introducao">
+		<section class="introducao-interna interna_contato">
 			<div class="container">
-				<h1>Bicicletas Feitas a Mão</h1>
-				<blockquote class="quote-externo">
-					<p>“não tenha nada em sua casa que você não considere útil ou acredita ser bonito”</p>
-					<cite>WILLIAM MORRIS</cite>
-				</blockquote>
-				<a href="produtos.html" class="btn">Orçamento</a>
-			</div>
-		</section>
-		
-		<section class="produtos container animar">
-			<h2 class="subtitulo">Produtos</h2>
-			<ul class="produtos_lista">
-
-				<li class="grid-1-3">
-					<div class="produtos_icone">
-						<img src="img/produtos/passeio.png" alt="Bikcraft Passeio">
-					</div>
-					<h3>Passeio</h3>
-					<p>Muito melhor do que passear pela orla a vidros fechados.</p>
-				</li>
-
-				<li class="grid-1-3">
-					<div class="produtos_icone">
-						<img src="img/produtos/esporte.png" alt="Bikcraft Esporte">
-					</div>
-					<h3>Esporte</h3>
-					<p>Mais rápida do que Forrest Gump, ninguém vai pegar você.</p>
-				</li>
-
-				<li class="grid-1-3">
-					<div class="produtos_icone">
-						<img src="img/produtos/retro.png" alt="Bikcraft Retrô">
-					</div>
-					<h3>Retrô</h3>
-					<p>O passado volta para lembrarmos o que devemos fazer no futuro.</p>
-				</li>
-
-			</ul>
-
-			<div class="call">
-				<p>clique aqui e veja os detalhes dos produtos</p>
-				<a href="produtos.html" class="btn btn-preto">Produtos</a>
-			</div>
-
-		</section>
-		<!-- Fecha Produtos -->
-
-		<section class="portfolio">
-			<div class="container">
-				<h2 class="subtitulo">Portfólio</h2>
-				<div class="portfolio_lista">
-					<div class="grid-8"><img src="img/portfolio/retro.jpg" alt="Bicicleta Retrô"></div>
-					<div class="grid-8"><img src="img/portfolio/passeio.jpg" alt="Bicicleta Passeio"></div>
-					<div class="grid-16"><img src="img/portfolio/esporte.jpg" alt="Bicicleta Esporte"></div>
-				</div>
-				<div class="call">
-					<p>conheça mais o nosso portfólio</p>
-					<a href="portfolio.html" class="btn">Portfólio</a>
-				</div>
+				<h1>Contato</h1>
+				<p>tire suas dúvidas com a gente</p>
 			</div>
 		</section>
 
-		<section class="qualidade container">
-			<h2 class="subtitulo">Qualidade</h2>
-			<img src="img/bikcraft-qualidade.png" alt="Bikcraft">
-			<ul class="qualidade_lista">
-				<li class="grid-1-3">
-					<h3>Durabilidade</h3>
-					<p>Sólida como pedra, leve como o vento e resistente como o diamante, são nossos diferenciais.</p>
-				</li>
-				<li class="grid-1-3">
-					<h3>Design</h3>
-					<p>Feitas sob medida para o melhor conforto e eficiência. Adaptamos a sua Bikcraft para o seu corpo.</p>
-				</li>
-				<li class="grid-1-3">
-					<h3>Sustentabilidade</h3>
-					<p>Além de ajudar a cuidar do meio ambiente, tirando carros da rua, toda a produção é sustentável.</p>
-				</li>
-			</ul>
-			<div class="call">
-				<p>conheça mais a nossa história</p>
-				<a href="sobre.html" class="btn btn-preto">Sobre</a>
+		<section class="contato container animar-interno">
+			<form action="enviar.php" method="post" name="form" class="formphp contato_form grid-8">
+				<label for="nome">Nome</label>
+				<input id="nome" name="nome" type="text">
+				<label for="email">E-mail</label>
+				<input id="email" name="email" type="text">
+				<label for="telefone">Telefone</label>
+				<input id="telefone" name="telefone" type="text">
+
+				<label class="nao-aparece">Se você não é um robô, deixe em branco.</label>
+				<input type="text" class="nao-aparece" name="leaveblank">
+				<label class="nao-aparece">Se você não é um robô, não mude este campo.</label>
+				<input type="text" class="nao-aparece" name="dontchange" value="http://" >
+
+				<label for="mensagem">Mensagem</label>
+				<textarea name="mensagem" id="mensagem"></textarea>
+
+				<button id="enviar" name="enviar" type="submit" class="btn btn-preto">Enviar</button>
+			</form>
+
+			<div class="contato_dados grid-8">
+				<h3>Dados</h3>
+				<span>+55 21 9999-9999</span>
+				<span>orcamento@bikcraft.com</span>
+				<span>Rua Ali Perto - Botafogo</span>
+				<span>Rio de Janeiro - RJ - Brasil</span>
+				<h3>Redes Sociais</h3>
+				<ul>
+					<li><a href="http://facebook.com" target="_blank"><img src="img/redes-sociais/facebook.png" alt="Facebook Bikcraft"></a></li>
+					<li><a href="http://instagram.com" target="_blank"><img src="img/redes-sociais/instagram.png" alt="Instagram Bikcraft"></a></li>
+					<li><a href="http://twitter.com" target="_blank"><img src="img/redes-sociais/twitter.png" alt="Twitter Bikcraft"></a></li>
+				</ul>
 			</div>
+		</section>
+
+		<section class="container contato_mapa">
+			<a href="http://google.com" target="_blank" class="grid-16"><img src="img/endereco-bikcraft.jpg" alt="Endereço da Bikcraft"></a>
 		</section>
 
 		<div class="quebra">
